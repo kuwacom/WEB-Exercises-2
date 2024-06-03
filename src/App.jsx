578815689f09df2a2aa9f2dc0aaba9ef9c7bc0f8
kuwacom <kuwa.com3@gmail.com>
2024-06-03 11:49:51 +0900

    update: clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Toolbar } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Todo from "./pages/Todo";
 
 function App() {
